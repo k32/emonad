175bda795cf8dd7a4c1c5dab2fdb7ea6e3600bbc
k32 <10274441+k32@users.noreply.github.com>
2023-01-29 22:08:44 +0100

    Initial commit

---
+++ src/emonad.erl
@@ -0,0 +1,95 @@
+-module(emonad).
+
+-export(['>>'/3, fmap/3, join/2]).
+
+-export([parse_transform/2]).
+
+-callback bind(m(A), fun((A) -> m(B))) -> m(B).
+
+-callback return(A) -> m(A).
+
+-callback nomatch(_A) -> m(_B).
+
+-optional_callbacks([nomatch/1]).
+
+-type m(_A) :: term().
+
+-compile({parse_transform, erlang_qq}).
+
+-include_lib("erlang_qq/include/erlang_qq.hrl").
+
+%%================================================================================
+%% Monad stuff
+%%================================================================================
+
+-spec '>>'(module(), m(_A), m(B)) -> m(B).
+'>>'(M, A, B) ->
+  M:bind(A, fun(_) -> B end).
+
+-spec fmap(module(), m(A), fun((A) -> B)) -> m(B).
+fmap(M, A, F) ->
+  M:bind(A, fun(Arg) -> M:return(F(Arg)) end).
+
+-spec join(module(), m(m(A))) -> m(A).
+join(M, A) ->
+  M:bind(A, fun(A_) -> A_ end).
+
+%%================================================================================
+%% Parse transform stuff
+%%================================================================================
+
+%-define(debug, true).
+
+-ifdef(debug).
+log(Fmt, Args) ->
+  io:format(user, Fmt ++ "~n", Args).
+-else.
+log(_, _) ->
+  ok.
+-endif.
+
+parse_transform(Forms, _Options) ->
+  log("Input forms:~n  ~p", [Forms]),
+  normal(Forms).
+
+%%================================================================================
+%% Internal functions
+%%================================================================================
+
+normal({lc, Loc, {op, _, '/', ?ATOM(_, do), Module__AST = ?ATOM(_, _)}, Forms}) -> %% Magic list comprehension
+  Ret = monadic(Module__AST, Forms),
+  log("Monadic at ~p:~n  ~p~nResult:~n  ~p", [Loc, Forms, Ret]),
+  Ret;
+normal(L) when is_list(L) -> %% Everything else
+  lists:map(fun normal/1, L);
+normal(T) when is_tuple(T) ->
+  list_to_tuple(lists:map(fun normal/1, tuple_to_list(T)));
+normal(T) ->
+  T.
+
+monadic(_Module, [A]) ->
+  A;
+monadic(Module__AST, [{generate, Line, Pattern__AST, Body__AST} | Rest]) ->
+  Var__AST = gensym(Line),
+  '$$'(Module__AST:bind(Body__AST,
+                         fun(Var__AST) ->
+                             case Var__AST of
+                               Pattern__AST -> '$'(monadic(Module__AST, normal(Rest)));
+                               _            -> Module__AST:nomatch(Var__AST)
+                             end
+                         end));
+monadic(Module__AST, [Match__AST = {match, Line, Pattern__AST, Body__AST} | Rest]) ->
+  {block, Line, [normal(Match__AST), monadic(Module__AST, normal(Rest))]};
+monadic(Module__AST, [Form__AST | Rest]) ->
+  '$$'(Module__AST:bind(Form__AST,
+                        fun(_) ->
+                            '$'(monadic(Module__AST, normal(Rest)))
+                        end)).
+
+gensym(Line) ->
+  case get(emonad_gensym) of
+    undefined -> N = 0;
+    N         -> ok
+  end,
+  put(emonad_gensym, N + 1),
+  {var, Line, list_to_atom("__EmonadTMPVar" ++ integer_to_list(N))}.
---
+++ test/emonad_trans_test.erl
@@ -0,0 +1,112 @@
+%%--------------------------------------------------------------------
+%% Copyright (c) 2023 k32. All Rights Reserved.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%--------------------------------------------------------------------
+-module(emonad_trans_test).
+
+-compile(nowarn_export_all).
+-compile(export_all).
+
+-compile({parse_transform, emonad}).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-import(emonad_list, [return/1]).
+
+trans_00_test() ->
+  Result = [do/emonad_list ||
+             A <- [a, b],
+             return(A)],
+  ?assertMatch([a, b], Result).
+
+trans_01_test() ->
+  Result = [do/emonad_list ||
+             A <- [a, b],
+             B <- [0, 1],
+             return({A, B})],
+  ?assertMatch([{a, 0}, {a, 1}, {b, 0}, {b, 1}], Result).
+
+trans_02_test() ->
+  Result = [do/emonad_list ||
+             A <- [a, b],
+             B <- [1, 2],
+             X = B - 1,
+             return({A, X})],
+  ?assertMatch([{a, 0}, {a, 1}, {b, 0}, {b, 1}], Result).
+
+trans_03_test() ->
+  Result = [do/emonad_list ||
+             X = 1,
+             Y = 2,
+             return({X, Y})],
+  ?assertMatch([{1, 2}], Result).
+
+trans_04_lc_inside_test() ->
+  Result = [do/emonad_list ||
+             X <- [A * 2 || A <- [1, 2]],
+             return(X)],
+  ?assertMatch([2, 4], Result).
+
+trans_05_branching_test() ->
+  Result = [do/emonad_list ||
+             X <- [1, 2],
+             case X of
+               1 -> return({X, one});
+               2 -> return({X, two})
+             end],
+  ?assertMatch([{1, one}, {2, two}], Result).
+
+trans_06_nesting_test() ->
+  Result = [do/emonad_list ||
+             X <- [1, 2],
+             case X of
+               1 -> [do/emonad_list ||
+                      Y <- [1, 2],
+                      return({X, Y})];
+               2 -> return({X, two})
+             end],
+  ?assertMatch([{1, 1}, {1, 2}, {2, two}], Result).
+
+trans_07_nesting_test() ->
+  Result = [do/emonad_list ||
+             X <- [1, 2],
+             [do/emonad_list ||
+               Y <- [1, 2],
+               return({X, Y})]],
+  ?assertMatch([{1, 1}, {1, 2}, {2, 1}, {2, 2}], Result).
+
+trans_08_nesting_test() ->
+  Result = [do/emonad_list ||
+             X = [do/emonad_list ||
+                   Y <- [1, 2],
+                   return(Y + 1)],
+             Y <- [a, b],
+             return({Y, X})],
+  ?assertMatch([{a, [2, 3]}, {b, [2, 3]}], Result).
+
+trans_09_nomatch_test() ->
+  Result = [do/emonad_list ||
+             A <- [a, b],
+             A <- [a, b],
+             return(A)],
+  ?assertMatch([a, {nomatch, b}, {nomatch, a}, b], Result).
+
+references() ->
+    _Ref = fun(A) ->
+               case A of
+                 1 -> A, A;
+                 _ -> A, A, A
+               end
+           end,
+  ok.
